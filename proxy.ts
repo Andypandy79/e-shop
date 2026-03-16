51f76c6cfad8d4e82d7f00e49fe2d6dc44a16dfa
Andypandy79 <78705049+Andypandy79@users.noreply.github.com>
2026-03-16 20:22:16 +0100

    proxy fix

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,4 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 
-import { NextRequest, NextResponse } from 'next/server';
-
-export function proxy(request: NextRequest) {
-  // your logic
-const auth = NextAuth(authConfig);
-  return NextResponse.next();
-}
-
-// export const { auth: middleware } = NextAuth(authConfig);
+export const proxy = NextAuth(authConfig);
