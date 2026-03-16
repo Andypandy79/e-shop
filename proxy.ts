64c4b59deb4b4ea40a80486c7630dd0c90f24a6e
Andypandy79 <78705049+Andypandy79@users.noreply.github.com>
2026-03-16 20:07:07 +0100

    proxy error

--- proxy.ts
+++ proxy.ts
@@ -5,8 +5,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function proxy(request: NextRequest) {
   // your logic
-
+const auth = NextAuth(authConfig);
   return NextResponse.next();
 }
 
-export const { auth: middleware } = NextAuth(authConfig);
+// export const { auth: middleware } = NextAuth(authConfig);
